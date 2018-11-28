80efeef4e5453bda4adfd858b534c2b4d3a50dee
Olivia Dimmer <Olivia.dimmer@gmail.com>
2018-11-27 19:02:24 -0600

    toggle content added

--- site.js
+++ site.js
@@ -1,42 +1,41 @@
 // console.log("hello World")
-//
-// (function(){
-//   // Check to make sure essential features are supported
-//   if (typeof document.querySelector === 'undefined') {
-//     // No? Then bye!!!
-//     return;
-//   }
 
-  document.addEventListener('DOMContentLoaded', function(){
+(function(){
+  // Check to make sure essential features are supported
+  if (typeof document.querySelector === "undefined") {
+    // No? Then bye!!!
+    return;
+  }
+
+  document.addEventListener("DOMContentLoaded", function(){
     // Diagnostic...make sure DOMContentLoaded has fired...
-    console.log('OMG the DOM has loaded!');
+    console.log("OMG the DOM has loaded!");
 
     // Grab the HTML element:
-    var html_tag = document.querySelector('html');
+    var html_tag = document.querySelector("html");
 
     // Go and remove the .nojs class from <html>...
-    html_tag.classList.remove('nojs');
+    html_tag.classList.remove("nojs");
 
     // And then add the .js class to <html>:
-    html_tag.classList.add('js');
-    var heading_label = 'Show Less';
-    var heading = document.querySelector('.show-more-section')
-    var showButton = document.querySelector('.button')
-    showButton.addEventListener('click',function(){
-      var content = document.querySelector('.show-more-section');
-      var label = document.querySelector('.button')
-
+    html_tag.classList.add("js");
+    var heading_label = "Show Less";
+    var heading = document.querySelector(".show-more-section")
+    var showButton = document.querySelector(".button")
+    showButton.addEventListener("click", function(){
+      var content = document.querySelector(".show-more-section");
+      var label = document.querySelector(".button")
+      content.classList.toggle("hidden");
+      content.classList.toggle("showstyle");
       if (label.textContent === heading_label) {
-        console.log("We are getting rid of content")
-        label.textContent = 'Show More';
-        content.style.display='none'
+        label.textContent = "Show More";
       }
       else {
         label.textContent = heading_label;
-        content.style.display='flex'
+        content.style.display="flex"
       }
     })
   });
-
+})();
 //   console.log("Hello from JavaScript! You have querySelector!");
 // })();
